267e026df31687578cb65b9d715b0a059a9428dc
chris <chris.svec@gmail.com>
2018-09-06 00:50:31 -0400

    Moved things over to gevent as a production-ish webserver, and added extra log handling.

--- app.py
+++ app.py
@@ -2,8 +2,28 @@
 
 # Flask is used to create a somewhat lightweight listening server
 from proxy.init import spawn_proxy
+from logging.config import dictConfig
+from gevent.pywsgi import WSGIServer
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '%(asctime)s - %(levelname)s - %(message)s'
+    }},
+    'handlers': {'stdout': {
+        'class': 'logging.StreamHandler',
+        'level': 'INFO',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'waitress': {
+        'level': 'INFO',
+        'handlers': ['stdout']
+    }
+})
 
 app = spawn_proxy()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    http_server = WSGIServer(('', 8080), app)
+    http_server.serve_forever()
